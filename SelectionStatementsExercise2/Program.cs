9e0e62ca989f9b405185a3d3e4ce837d66b95661
Janine Masunzu <jamasunzu99@gmail.com>
2023-06-21 14:50:23 -0500

    done with excercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subjest");
+            String subjest = Console.ReadLine();
+            switch (subjest.ToLower())
+            {
+                case "science":
+                    Console.WriteLine("I love Sciense");
+                    break;
+                case "math":
+                    Console.WriteLine("I hate Math");
+                    break;
+                case "history":
+                    Console.WriteLine("History is The best");
+                    break;
+                case "english":
+                    Console.WriteLine("English is fun");
+                    break;
+                case "geography":
+                    Console.WriteLine("Geography is interesting");
+                    break;
+                default:
+                    Console.WriteLine("I don't recognize that subject. ");
+                    break;
+
+
+
+            }
         }
     }
 }
